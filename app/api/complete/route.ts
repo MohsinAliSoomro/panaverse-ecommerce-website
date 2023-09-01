7ef053ca05e97931a760c11bbddca409be967b46
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2023-09-01 23:39:25 +0500

    add headers

--- app/api/complete/route.ts
+++ app/api/complete/route.ts
@@ -1,22 +1,25 @@
 import Stripe from "stripe";
 import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
 // import { buffer } from "micro";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
-const endpointSecret =
-  "whsec_7a050b012603667a044f6f49e493c9f759e84e289ed6ec2ec975091b3feba649";
+// const endpointSecret =
+//   "whsec_7a050b012603667a044f6f49e493c9f759e84e289ed6ec2ec975091b3feba649";
 
+const endpointSecret = "we_1NlXbAFxAUz38nIubf8ERTl3";
 export async function POST(request: NextRequest) {
   try {
     console.log("Start event");
     const body = await request.text();
     console.log("after body", { body });
-    const sig = request.headers.get("stripe-signature")!;
+    const sig = headers().get("stripe-signature")!;
     console.log("after sig", { sig });
     let event: Stripe.Event;
     try {
+      if (!sig || !endpointSecret) return;
       event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
       console.log("INSIDE A EVENT", { event });
     } catch (err) {
