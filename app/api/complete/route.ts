f555110c2f6fe2ec6ac84e7b54e30f697b7e5b05
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2023-09-03 11:15:57 +0500

    changes

--- app/api/complete/route.ts
+++ app/api/complete/route.ts
@@ -7,68 +7,48 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export async function POST(request: NextRequest) {
+  const body = await request.text();
+  const endpointSecret = process.env.STRIPE_SECRET_WEBHOOK_KEY!;
+  const sig = headers().get("stripe-signature") as string;
+  console.log({ sig, endpointSecret });
+  let event: Stripe.Event;
   try {
-    const body = await request.text();
-    const endpointSecret = process.env.STRIPE_SECRET_WEBHOOK_KEY!;
-    const sig = headers().get("stripe-signature") as string;
-    console.log({ sig });
-    let event: Stripe.Event;
-    try {
-      event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-    } catch (err) {
-      return NextResponse.json(`Webhook Error: ${err}`);
-    }
-    console.log({ event });
-    switch (event.type) {
-      case "checkout.session.async_payment_failed":
-        const checkoutSessionAsyncPaymentFailed = event.data.object;
-        console.log({ checkoutSessionAsyncPaymentFailed });
-        // Then define and call a function to handle the event checkout.session.async_payment_failed
-        break;
-      case "checkout.session.async_payment_succeeded":
-        const checkoutSessionAsyncPaymentSucceeded = event.data.object;
-        console.log({ checkoutSessionAsyncPaymentSucceeded });
-
-        // Then define and call a function to handle the event checkout.session.async_payment_succeeded
-        break;
-      case "checkout.session.completed":
-        const checkoutSessionCompleted: any = event.data.object;
-        const response1 = await db
-          .insert(OrderTable)
-          .values({
-            userId: checkoutSessionCompleted?.metadata.userId,
-            itemCount: 1,
-            total: checkoutSessionCompleted?.amount_total as any,
-            isComplete: true,
-          })
-          .returning();
-        console.log({ response1, checkoutSessionCompleted });
-        // Then define and call a function to handle the event checkout.session.completed
-        break;
-      case "payment_intent.created":
-        console.log("payment intent create", event.data);
-        // ... handle other event types
-        break;
-      case "payment_intent.succeeded":
-        console.log("payment intent success", event.data);
-        break;
-      case "charge.succeeded":
-        console.log("charge success intent success");
-
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-    return NextResponse.json("RESPONSE EXECUTE");
-  } catch (error) {
-    console.log({ error });
-    return NextResponse.json(
-      {
-        error: {
-          message: `Method Not Allowed`,
-        },
-      },
-      { status: 405 }
-    );
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  } catch (err) {
+    console.log({ err });
+    return NextResponse.json(`Webhook Error: ${err}`);
   }
+  // const session = event.data.object as Stripe.Checkout.SessionsResource;
+
+  console.log({ event });
+  switch (event.type) {
+    case "checkout.session.async_payment_failed":
+      const checkoutSessionAsyncPaymentFailed = event.data.object;
+      console.log({ checkoutSessionAsyncPaymentFailed });
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      break;
+    case "checkout.session.async_payment_succeeded":
+      const checkoutSessionAsyncPaymentSucceeded = event.data.object;
+      console.log({ checkoutSessionAsyncPaymentSucceeded });
+
+      // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+      break;
+    case "checkout.session.completed":
+      const checkoutSessionCompleted: any = event.data.object;
+      const response1 = await db
+        .insert(OrderTable)
+        .values({
+          userId: checkoutSessionCompleted?.metadata.userId,
+          itemCount: 1,
+          total: checkoutSessionCompleted?.amount_total as any,
+          isComplete: true,
+        })
+        .returning();
+      console.log({ response1 });
+      // Then define and call a function to handle the event checkout.session.completed
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  return NextResponse.json({ message: "RESPONSE EXECUTE", data: event });
 }
